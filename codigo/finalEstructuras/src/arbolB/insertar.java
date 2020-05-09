cf946ed34a238be33f8ace5e7eb2b144dc4cf3ca
davidCunocUSAC <davidrodolfo-martinezmiranda@cunoc.edu.gt>
2020-05-08 22:27:51 -0600

    ya expande hacia arriaba, falta arreglar que al insertar hacia arriba este coloque los hijos izquierdos y derechos como debe

--- codigo/finalEstructuras/src/arbolB/insertar.java
+++ codigo/finalEstructuras/src/arbolB/insertar.java
@@ -44,12 +44,12 @@ public class insertar {
                     break;
                 }else {
                     System.out.println("Tiene hijo a la izquierda");
-                    indices[i].hijoI = insetarDato(valor, indices[i].hijoI,false);
+                    indices = insetarDato(valor, indices[i].hijoI,false);
                     return indices;
                 }
             }else if(valor > indices[i].dato){
                 if(indices[i].hijoD != null){
-                    indices[i].hijoD = insetarDato(valor,indices[i].hijoD,false);
+                    indices = insetarDato(valor,indices[i].hijoD,false);
                     return indices;
                 }
             }
@@ -91,15 +91,42 @@ public class insertar {
             nuevaEstructura.dato = datos[2];
             nuevaEstructura.raiz = obtenerRaiz(indices[2]);
             int nuevoContador = 0;
-            if(indices[0].anterior == null){
+            if(indices[0].anterior == null || indices[0].anterior[4] != null){
                 estructura nuevoAnterior [] = new estructura[5];
                 nuevoAnterior [0] = nuevaEstructura;
                 nuevoAnterior [0].anterior = nuevoAnterior;
                 indices = nuevoAnterior;
             }else {
-                System.out.println("el anterior no es nulo");                
-                insertar.insetarDato(datos[2], indices[0].anterior,true);
+                
+                int limite2 = 0;
+                int datosInsertar = 0;
+                for (int i = 0; i < indices[0].anterior.length; i++) {
+                    if(nuevaEstructura.dato > indices[0].anterior[i].dato){
+                        datosInsertar++;
+                    }else if(nuevaEstructura.dato < indices[0].anterior[i].dato)break;
+                }
+                limite2 = nuevaEstructura.espaciosMaximosPermitidos - datosInsertar;
+                estructura aux [] = new estructura[limite2];
+                for (int i = 0; i < aux.length; i++) {
+                    if(indices[0].anterior[i+datosInsertar]!=null){
+                        aux[i] = indices[0].anterior[i+datosInsertar];
+                    }else break;
+                }
+                for (int i = 0; i < nuevaEstructura.espaciosMaximosPermitidos; i++) {
+                    if(i == 0){
+                        indices[0].anterior[i+datosInsertar] = nuevaEstructura;
+                        aux[i].hijoI = nuevaEstructura.hijoD;
+                        aux[i].hijoD = nuevaEstructura.hijoI;
+                    }
+                    else{
+                        if(aux[i-1] == null)break;
+                        indices[0].anterior[i+datosInsertar] = aux[i-1];                        
+                    }
+                }
+                nuevaEstructura.anterior = indices[0].anterior;
+                indices = indices[0].anterior;
             }
+            
             for (int i = 0; i < indices.length; i++) {
                 if(i<2){
                     nuevaEstructura.hijoI[i] = new estructura();
@@ -112,11 +139,11 @@ public class insertar {
                     nuevaEstructura.hijoD[nuevoContador].dato = datos[i];
                     nuevaEstructura.hijoD[nuevoContador].padre = nuevaEstructura;
                     nuevaEstructura.hijoD[nuevoContador].raiz = obtenerRaiz(indices[2]);
+                    nuevaEstructura.hijoD[nuevoContador].anterior = nuevaEstructura.anterior;
                     nuevoContador++;
                 }
             }
-            System.out.println("su anterior es "+indices[0].anterior);
-            
+            System.out.println("su anterior es "+indices[0].anterior);           
             return indices;            
         }
         
